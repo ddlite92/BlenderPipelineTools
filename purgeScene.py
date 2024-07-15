309951f18011fb8c02b0f41c438da79714b3d1a4
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-07-15 10:30:36 +0800

    Update purgeScene.py
    Update script with msg box

--- purgeScene.py
+++ purgeScene.py
@@ -1,11 +1,13 @@
 import bpy
 
-bpy.ops.outliner.orphans_purge(do_local_ids=do_local_ids, do_linked_ids=do_linked_ids, do_recursive=do_recursive)
+bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
 
-# Optional: Set arguments here (default: all True)
-local_ids = True
-linked_ids = True
-recursive = True
+def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
 
-# Execute the purge function
-purge_orphans(local_ids, linked_ids, recursive)
+    def draw(self, context):
+        self.layout.label(text=message)
+
+    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+
+#Shows a message box with a specific message 
+ShowMessageBox("Orphan data purged successfully!")
