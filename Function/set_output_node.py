653d0409c710b29485cb3d3df1c8c2a6841aea4a
ddlite92 <junki2497@yahoo.com>
2024-11-19 22:39:54 +0800

    Update set_output_node.py
    fixed output node function with
    -  removing filename function
    -  return current active scene name

--- Function/set_output_node.py
+++ Function/set_output_node.py
@@ -7,14 +7,13 @@ from pathlib import Path
 """
 
 Still WIP
-- iterate scene
 - iterate node label
 - function to get 
-    - filename
-    - folder
-    - scene
-- scene_names function get the first scene only, need to change to index 1,2,3,4 respectively
-
+    - filename ( to be test )
+    - folder ( to be test )
+    - scene ( done )
+- scene_names function get the first scene only, need to change to index 1,2,3,4 respectively ( fixed )
+- function filename removed due non use
 """
 path = r"M:\BBBGS2_Gentar_Arc\2_Episode\Episode_15\6_Post\3_Output\1_Render\PART B\_MONSTA"
 
@@ -23,11 +22,6 @@ filepath = bpy.data.filepath
 mainScreen = bpy.context.window
 node_tree = bpy.context.scene.node_tree
 links = node_tree.links
-
-def filename():
-    filename = bpy.path.basename(bpy.context.blend_data.filepath)
-    filename = filename.split('.')[0].split('_')[:-1]
-    return filename
 
 def folder():
     filename = bpy.path.basename(bpy.context.blend_data.filepath)
@@ -38,17 +32,15 @@ def folder():
 def scene_name():
     scene_names = [scene.name for scene in bpy.data.scenes]
     for scene_name in scene_names:
-        scene = bpy.data.scenes.get(scene_name)
-        return scene_names[0]
-    # scene_names get the first scene, need to change to index 1,2,3,4 respectively
+        scene = bpy.context.scene.name
+        return scene
     
-
 bpy.context.scene.render.filepath = os.path.join(path, folder(), scene_name(), "Beauty", "Beauty_" )
 
 def output_node():
     for node in mainScreen.scene.node_tree.nodes:
         nodeBeauty = node_tree.nodes.get("BeautyOutput")
-            
+        current_scene_name = scene_name()
         if not nodeBeauty:
             beautyNode = node_tree.nodes.new(type="CompositorNodeOutputFile")
             beautyNode.location = 500, 1000
@@ -65,7 +57,7 @@ def output_node():
             filename = node.file_slots.keys()[0]
             label_name = node.label.split("_")[-1]
             node.file_slots[filename].path = label_name + '_'
-            node.base_path = path + "\\" + folder() + "\\" + scene_name() + "\\" + node.label + "\\"
+            node.base_path = path + "\\" + folder() + "\\" + current_scene_name + "\\" + node.label + "\\"
             node.format.color_mode = "RGB" 
         
         elif "Matte" in node.label:
@@ -73,7 +65,7 @@ def output_node():
             filename = node.file_slots.keys()[0]
             label_name = node.label.split("_")[-1]
             node.file_slots[filename].path = label_name + '_'
-            node.base_path = path + "\\" + folder() + "\\" + scene_name() + "\\" + node.label + "\\"
+            node.base_path = path + "\\" + folder() + "\\" + current_scene_name  + "\\" + node.label + "\\"
             node.format.file_format = "OPEN_EXR_MULTILAYER"
             node.format.color_mode = "RGBA"
             node.format.color_depth = "32"
@@ -83,7 +75,7 @@ def output_node():
             filename = node.file_slots.keys()[0]
             label_name = node.label.split("_")[-1]
             node.file_slots[filename].path = label_name + '_'
-            node.base_path = path + "\\" + folder() + "\\" + scene_name() + "\\" + node.label + "\\"
+            node.base_path = path + "\\" + folder() + "\\" + current_scene_name + "\\" + node.label + "\\"
             node.format.file_format = "PNG"
             node.format.color_mode = "RGBA"
             node.format.color_depth = "16"
