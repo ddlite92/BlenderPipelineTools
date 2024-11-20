9c08fa30398443e3a45e3b4425df3a70abc7affd
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-11-20 14:29:34 +0800

    Update get_anim_file.py
    - change return value as raw string for windows path

--- Function/get_anim_file.py
+++ Function/get_anim_file.py
@@ -11,15 +11,18 @@ from bpy.app.handlers import persistent
 from bpy.utils import previews, register_class, unregister_class
 from pathlib import Path
 
+
+# Finds the animation blend file based on the current file's location.
+# Code edited to read windows path and return as raw string
+
 context = bpy.context
 bpath = bpy.path
 filepath = bpy.data.filepath
 
-
 def get_anim_blendfile():
-    current_path = bpy.data.filepath
-    anim_file_dir = os.path.dirname(current_path)
-    for file in os.listdir(anim_file_dir) :
-        if file.endswith("_ANIM.blend") : 
-            anim_file = os.path.join(anim_file_dir, file)
-            return anim_file
+  current_path = bpy.data.filepath
+  anim_file_dir = os.path.dirname(current_path)
+  for file in os.listdir(anim_file_dir):
+    if file.endswith("_ANIM.blend"):
+      anim_file = os.path.join(anim_file_dir, r"./" + file)  # Use raw string for Windows path
+      return anim_file
