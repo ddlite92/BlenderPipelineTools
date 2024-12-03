706f9941a39c5a7136586f832f0088042adc56f2
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-12-03 22:31:09 +0800

    remove unwanted function
    - remove replace_work_to_render function

--- Function/set_output_node_linux.py
+++ Function/set_output_node_linux.py
@@ -9,28 +9,24 @@ filepath =  bpy.data.filepath
 def get_1st_lvl():
     filepath =  bpy.data.filepath
     full_path = os.path.dirname(filepath)
-    print("full path: ", full_path)
-    return(full_path)  
-
-get_1st_lvl()
+    base_path = "Documents/ScriptPY/Monsta/Blender/Path_Testing"
+    relative_path = os.path.relpath(full_path, base_path)
+    return(relative_path)  
 
 def get_workpath():
     #filepath = bpy.data.filepath
-    blendpath = '\\'.join(filepath.split('\\')[7:8]) + '\\'
-    print('blendpath: ', blendpath)
+    blendpath = '/'.join(filepath.split('/')[7:8]) + '/'
     return(blendpath)   
 
 def get_shotname():
     filename = bpy.path.basename(bpy.context.blend_data.filepath)
     basename, extension = os.path.splitext(filename)
     new_basename = basename[:-7]  # Remove "_Render"
-    print('new_basename: ', new_basename)
     return new_basename
-        
+       
 def set_renderpath():
     final = os.path.join(get_1st_lvl())
     final = final.replace("2_Work", "3_Output")
-    print('final: ', final)
     return(final)
 
 
@@ -43,15 +39,15 @@ def set_main_node():
     
     scenes = [s for s in local_scenes]
     for scn in scenes:
-            scn.render.filepath = path.join(renderpath + '\\' + shotname)
+            scn.render.filepath = path.join(renderpath + '/' + shotname)
     
     output_nodes = [n for s in scenes for n in s.node_tree.nodes if n.type == "OUTPUT_FILE"]
     for node in output_nodes:
         if "_Matte" in node.label:
             label_name = node.label.split("_Matte")[0]
             pass_name = 'Matte'
-            splitOutput = os.path.join(renderpath + '\\' + shotname + '_')
-            node.base_path = os.path.join(renderpath + '\\' + label_name + '\\' + pass_name + '\\')
+            splitOutput = os.path.join(renderpath + '/' + shotname + '_')
+            node.base_path = os.path.join(renderpath + '/' + label_name + '/' + pass_name + '/')
             node.format.file_format = "OPEN_EXR_MULTILAYER"
             node.format.color_mode = "RGBA"
             node.format.color_depth = "32"
@@ -60,7 +56,7 @@ def set_main_node():
             label_name = node.label.split("_Emission")[0]
             pass_name = node.label.split("_")[1]
             filename = node.file_slots.keys()[0]
-            node_output = os.path.join(renderpath + '\\' + label_name + '\\' + pass_name + '\\')
+            node_output = os.path.join(renderpath + '/' + label_name + '/' + pass_name + '/')
             node.base_path = node_output
             node.file_slots[filename].path = pass_name + '_'
             node.format.file_format = "PNG"
@@ -71,7 +67,7 @@ def set_main_node():
             #label_name = node.label
             pass_name = node.label.split("_")[0]
             filename = node.file_slots.keys()[0]
-            node_output = os.path.join(renderpath + '\\' + pass_name + '\\')
+            node_output = os.path.join(renderpath + '/' + pass_name + '/')
             node.base_path = node_output
             node.file_slots[filename].path = pass_name + '_'
             node.format.file_format = "PNG"
@@ -82,7 +78,7 @@ def set_main_node():
             #label_name = node.label
             pass_name = node.label.split("_")[0]
             filename = node.file_slots.keys()[0]
-            node_output = os.path.join(renderpath + '\\' + pass_name + '\\')
+            node_output = os.path.join(renderpath + '/' + pass_name + '/')
             node.base_path = node_output
             node.file_slots[filename].path = pass_name + '_'
             node.format.file_format = "PNG"
@@ -94,11 +90,11 @@ def set_main_node():
             label_name = node.label.split("_")[0]
             pass_name = node.label.split("_")[1]
             filename = node.file_slots.keys()[0]
-            node_output = os.path.join(renderpath + '\\' + label_name + '\\' + pass_name + '\\')
+            node_output = os.path.join(renderpath + '/' + label_name + '/' + pass_name + '/')
             node.base_path = node_output
             node.file_slots[filename].path = pass_name + '_'
             node.format.file_format = "PNG"
             node.format.color_depth = "16" 
                 
 set_main_node()
-
+            
