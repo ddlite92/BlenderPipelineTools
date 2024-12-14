d24268751f175d5f7cf72d23a8a02d535c0480ab
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-12-14 12:20:33 +0800

    change global cam
    - global changed from scene BG to RIM.

--- monstaPanel_pass.py
+++ monstaPanel_pass.py
@@ -154,7 +154,7 @@ class SetCamera(bpy.types.Operator):
     
     def execute(self, context):
         scenes = bpy.data.scenes
-        global_camera = bpy.data.scenes["BG"].camera
+        global_camera = bpy.data.scenes["RIM"].camera
         for scn in bpy.data.scenes:
             scn.camera = global_camera
 
