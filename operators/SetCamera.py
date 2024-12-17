a2bcddae4098d63b31e7153444e5760fae658745
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-12-18 07:19:48 +0800

    added export function

--- operators/SetCamera.py
+++ operators/SetCamera.py
@@ -3,10 +3,14 @@ class SetCamera(bpy.types.Operator):
     bl_label = "Set Camera"
     
     def execute(self, context):
+        '''
+        Set camera from _RIM scene and set frame from data extracted from the camera
+        '''
         scenes = bpy.data.scenes
         global_camera = bpy.data.scenes["_RIM"].camera
         frame_start = bpy.data.scenes["_RIM"].frame_start
         frame_end = bpy.data.scenes["_RIM"].frame_end
+
         for scn in bpy.data.scenes:
             scn.camera = global_camera
             scn.frame_start = frame_start
