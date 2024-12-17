a4543ed5a961d255d51f1d6fbf135ff3144e378a
ddlite92 <85956107+ddlite92@users.noreply.github.com>
2024-12-17 11:32:10 +0800

    RIM changed to _RIM

--- monstaPanel_pass.py
+++ monstaPanel_pass.py
@@ -156,10 +156,10 @@ class SetCamera(bpy.types.Operator):
     
     def execute(self, context):
         scenes = bpy.data.scenes
-        global_camera = bpy.data.scenes["RIM"].camera
-        frame_start = bpy.data.scenes["RIM"].frame_start
-        frame_end = bpy.data.scenes["RIM"].frame_end
-        global_camera = bpy.data.scenes["RIM"].camera
+        global_camera = bpy.data.scenes["_RIM"].camera
+        frame_start = bpy.data.scenes["_RIM"].frame_start
+        frame_end = bpy.data.scenes["_RIM"].frame_end
+        global_camera = bpy.data.scenes["_RIM"].camera
         for scn in bpy.data.scenes:
             scn.camera = global_camera
             scn.frame_start = frame_start
--- operators/SetCamera.py
+++ operators/SetCamera.py
@@ -4,9 +4,9 @@ class SetCamera(bpy.types.Operator):
     
     def execute(self, context):
         scenes = bpy.data.scenes
-        global_camera = bpy.data.scenes["RIM"].camera
-        frame_start = bpy.data.scenes["RIM"].frame_start
-        frame_end = bpy.data.scenes["RIM"].frame_end
+        global_camera = bpy.data.scenes["_RIM"].camera
+        frame_start = bpy.data.scenes["_RIM"].frame_start
+        frame_end = bpy.data.scenes["_RIM"].frame_end
         for scn in bpy.data.scenes:
             scn.camera = global_camera
             scn.frame_start = frame_start
